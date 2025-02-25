8a200dbd4147823d04da7d37620b7b8062fdee19
MihneaS16 <seitoarumihnea@gmail.com>
2025-02-25 19:58:15 +0200

    Removed unnecessary comments and refactored the code

--- client/src/components/UserProfile.jsx
+++ client/src/components/UserProfile.jsx
@@ -3,31 +3,32 @@ import { useParams, useNavigate } from "react-router-dom";
 import userService from "../utils/userService";
 
 export default function UserProfile() {
-    const {id} = useParams();
-    const [user, setUser] = useState(undefined);
+  const { id } = useParams();
+  const [user, setUser] = useState(undefined);
 
-    useEffect(() => {
-        const fetchUser = async () => {
-            try {
-                const user = await userService.getUser(id);
-                setUser(user);
-            } catch(error) {
-                console.error('Error fetching user', error);
-            }
-        }
-        fetchUser();
-    }, []);
+  useEffect(() => {
+    const fetchUser = async () => {
+      try {
+        const user = await userService.getUser(id);
+        setUser(user);
+      } catch (error) {
+        console.error("Error fetching user", error);
+      }
+    };
+    fetchUser();
+  }, []);
 
-    return (
-        <>
-        {user === undefined ? <h1>You are not allowed to do this</h1> :
-            <div className="UserInfo">
-                <h2>Email: {user.email}</h2>
-                <h2>Username: {user.username}</h2>
-            </div>
-        }
-        <a href="/">Go Back</a>
-        </>
-        
-    );
+  return (
+    <>
+      {user === undefined ? (
+        <h1>You are not allowed to do this</h1>
+      ) : (
+        <div className="UserInfo">
+          <h2>Email: {user.email}</h2>
+          <h2>Username: {user.username}</h2>
+        </div>
+      )}
+      <a href="/">Go Back</a>
+    </>
+  );
 }
